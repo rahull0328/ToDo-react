03e6e3f12b53584f232ddd7737462c5fb9a4dadc
rahull0328 <rahull0328@users.noreply.github.com>
2024-12-22 00:06:18 +0530

    Completed CRD Operations on Notes

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import checked from '../assets/checked.png'
 import delete_img from '../assets/delete.png'
+import not_tick from '../assets/not_tick.png'
+import tick from '../assets/tick.png'
 
-const TodoItems = () => {
+const TodoItems = ({text, id, isComplete, deleteTodo, toogle}) => {
   return (
     <div className='flex items-center my-3 gap-2'>
-        <div className='flex flex-1 items-center cursor-pointer'>
-            <img src={checked} alt="task_complete" className='w-7' />
-            <p className='text-slate-700 ml-4 text-[17px]'>Todo List</p>
+        <div className='flex flex-1 items-center cursor-pointer' onClick={() => {toogle(id)}}>
+            <img src={isComplete ? tick : not_tick} alt="task_complete" className='w-7' />
+            <p className={`text-slate-700 ml-4 text-[17px] decoration-slate-500 ${isComplete ? "line-through": ""}`}>{text}</p>
         </div>
-        <img src={delete_img} alt="delete_img" className='w-7 cursor-pointer' />
+        <img src={delete_img} onClick={() => {deleteTodo(id)}} alt="delete_img" className='w-7 cursor-pointer' />
     </div>
   )
 }
